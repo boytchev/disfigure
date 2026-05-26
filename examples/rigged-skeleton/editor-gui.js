b7e043e4b4320ec117bfd3c9c27b7643a0c940fc
boytchev <70705048+boytchev@users.noreply.github.com>
2026-05-26 07:50:46 +0300

    recovered some legacy examples

---
+++ examples/rigged-skeleton/editor-gui.js
@@ -0,0 +1,602 @@
+﻿
+// disfigure
+//
+// module to construct gui
+
+
+
+import * as THREE from "three";
+import * as lil from "three/addons/libs/lil-gui.module.min.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { uniform } from "three/tsl";
+
+
+
+const DEBUG = false;
+const DEBUG_JOINT = 0;
+
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color( 'whitesmoke' );
+
+
+
+var camera = new THREE.PerspectiveCamera( 30, innerWidth/innerHeight );
+camera.position.set( -2, 1, 3.5 );
+camera.lookAt( scene.position );
+
+
+
+var renderer = new THREE.WebGPURenderer( { antialias: true } );
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.setSize( innerWidth, innerHeight );
+document.body.appendChild( renderer.domElement );
+
+window.addEventListener( "resize", ( /*event*/ ) => {
+
+	camera.aspect = innerWidth/innerHeight;
+	camera.updateProjectionMatrix( );
+	renderer.setSize( innerWidth, innerHeight );
+
+} );
+
+
+
+var controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.zoomSpeed = 10;
+//controls.autoRotate = true;
+//controls.autoRotateSpeed = 0.5;
+
+
+
+var ambientLight = new THREE.AmbientLight( 'white', 1+2 );
+scene.add( ambientLight );
+
+
+
+var light = new THREE.DirectionalLight( 'white', 2+1 );
+light.position.set( 0, 0, 1 );
+scene.add( light );
+
+
+
+
+
+var pivot = new THREE.AxesHelper();
+
+
+var options = {
+	animate: false,
+};
+
+
+
+var debug = {
+	isolated: false,
+	randomize: rigRandomModel,
+};
+var posture = {};
+var skeleton = {};
+var model = new THREE.Group();
+var gui;
+
+function createGui( skeletonData, postureData, modelObject ) {
+
+	skeleton = skeletonData;
+
+	posture = postureData;
+	posture.select = uniform( DEBUG?DEBUG_JOINT:0, 'int' ); // 0..24
+	posture.isolated = uniform( DEBUG?1:0, 'int' ); // 0 or 1
+	model = modelObject;
+
+
+	gui = new lil.GUI(); // global gui
+	gui.domElement.style.marginRight = 0;
+
+	var mfolder = gui.addFolder( 'DEBUG' );
+
+	if ( !DEBUG ) mfolder.close();
+
+	mfolder.add( posture.select, 'value', {
+		Nothing: 0,
+		Head: 1, Chest: 2, Waist: 3,
+		Hip: 11, Hip2: 15, Leg: 12, Knee: 13, Ankle: 14, Foot: 16,
+		Arm: 21, Elbow: 22, Forearm: 23, Wrist: 24,
+	} ).name( 'Show' ).onChange( showPivotPoint );
+
+	if ( DEBUG ) {
+		// mfolder.add( skeleton.hipLeftPos.value, 'x', 0., 1 ).name( 'px' ).onChange( changePivotPoint );
+		// mfolder.add( skeleton.hipLeftPos.value, 'y', 1, 3 ).name( 'py' ).onChange( changePivotPoint );
+		// mfolder.add( skeleton.hipLeftPos.value, 'z', -0.5, 0.5 ).name( 'pz' ).onChange( changePivotPoint );
+		// mfolder.add( skeleton.hipLeftSpan.value, 'x', 1, 3 ).name( 'sx' ).onChange( changePivotPoint );
+		// mfolder.add( skeleton.hipLeftSpan.value, 'y', 1, 2 ).name( 'sy' ).onChange( changePivotPoint );
+		//		mfolder.add( skeleton.hipLeftSpan.value, 'z', 1, 3 ).name( 'sz' ).onChange( changePivotPoint );
+		//		mfolder.add( skeleton.hipLeftSpan.value, 'w', -2, 2 ).name( 'sw' ).onChange( changePivotPoint );
+
+	}
+
+	mfolder.add( debug, 'isolated', false ).name( 'Isolated' ).onChange( ()=>{
+
+		posture.isolated.value = debug.isolated?1:0;
+
+	} );
+	mfolder.add( options, 'animate', false ).name( 'Animate' );
+	mfolder.add( debug, 'randomize' ).name( 'Randomize' );
+
+	function html( name, icon, classes='' ) {
+
+		return `<div class="padded ${classes}">${name} &nbsp; <span class="icon">${icon}</span></div>`;
+
+	}
+
+
+	mfolder = gui.addFolder( 'TORSO' );//.close();
+	{
+
+		mfolder.add( posture.head.value, 'x', -0.7, 0.5 ).name( html( 'Head', '&#x2195;' ) );
+		mfolder.add( posture.head.value, 'z', -0.5, 0.5 ).name( html( '', '&#x21B6;' ) );
+		mfolder.add( posture.head.value, 'y', -1, 1 ).name( html( '', '&#x2194;' ) );
+
+		mfolder.add( posture.chest.value, 'x', -0.7, 0.4 ).name( html( 'Chest', '&#x2195;', 'border' ) );
+		mfolder.add( posture.chest.value, 'z', -0.5, 0.5 ).name( html( '', '&#x21B6;' ) );
+
+		mfolder.add( posture.waist.value, 'x', -1.0, 0.6 ).name( html( 'Waist', '&#x2195;', 'border' ) );
+		mfolder.add( posture.waist.value, 'z', -0.5, 0.5 ).name( html( '', '&#x21B6;' ) );
+		mfolder.add( posture.waist.value, 'y', -1, 1 ).name( html( '', '&#x2194;' ) );
+
+	}
+
+	mfolder = gui.addFolder( 'LEFT LEG' ).close();
+	{
+
+		mfolder.add( posture.hipLeft.value, 'x', -0.6, 2.7 ).name( html( 'Leg', '&#x2195;' ) );
+		mfolder.add( posture.hipLeft.value, 'z', -2.4, 0.2 ).name( html( '', '&#x21BA;' ) );
+		mfolder.add( posture.hip2Left.value, 'y', -1.4, 1.4 ).name( html( '', '&#x2194;' ) );
+
+		mfolder.add( posture.kneeLeft.value, 'x', -2.6, 0 ).name( html( 'Knee', '&#x2195;', 'border' ) );
+
+		mfolder.add( posture.ankleLeft.value, 'x', -1, 0.7 ).name( html( 'Ankle', '&#x2195;', 'border' ) );
+		mfolder.add( posture.legLeft.value, 'y', -1, 1 ).name( html( '', '&#x2194;' ) );
+		mfolder.add( posture.ankleLeft.value, 'z', -0.5, 0.5 ).name( html( '', '&#x21BA;' ) );
+
+		mfolder.add( posture.footLeft.value, 'x', -0.3, 0.6 ).name( html( 'Foot', '&#x2195;', 'border' ) );
+
+	}
+
+	mfolder = gui.addFolder( 'RIGHT LEG' ).close();
+	{
+
+		mfolder.add( posture.hipRight.value, 'x', -0.6, 2.7 ).name( html( 'Leg', '&#x2195;' ) );
+		mfolder.add( posture.hipRight.value, 'z', -0.2, 2.4 ).name( html( '', '&#x21BB;' ) ); // swapped
+		mfolder.add( posture.hip2Right.value, 'y', -1.4, 1.4 ).name( html( '', '&#x2194;' ) );
+
+		mfolder.add( posture.kneeRight.value, 'x', -2.6, 0 ).name( html( 'Knee', '&#x2195;', 'border' ) );
+
+		mfolder.add( posture.ankleRight.value, 'x', -1, 0.7 ).name( html( 'Ankle', '&#x2195;', 'border' ) );
+		mfolder.add( posture.legRight.value, 'y', -1, 1 ).name( html( '', '&#x2194;' ) );
+		mfolder.add( posture.ankleRight.value, 'z', -0.5, 0.5 ).name( html( '', '&#x21BB;' ) );
+
+		mfolder.add( posture.footRight.value, 'x', -0.3, 0.6 ).name( html( 'Foot', '&#x2195;', 'border' ) );
+
+	}
+
+	mfolder = gui.addFolder( 'LEFT ARM' ).close();
+	{
+
+		mfolder.add( posture.armLeft.value, 'z', -1.5, 1.7 ).name( html( 'Arm', '&#x2195;' ) );
+		mfolder.add( posture.armLeft.value, 'y', -0.5, 2 ).name( html( '', '&#x2194;' ) );
+		mfolder.add( posture.armLeft.value, 'x', -0.5, 0.5 ).name( html( '', '&#x21BB;' ) );
+
+		mfolder.add( posture.elbowLeft.value, 'y', 0, 2.7 ).name( html( 'Elbow', '&#x2195;', 'border' ) );
+
+		mfolder.add( posture.wristLeft.value, 'z', -1.4, 1.4 ).name( html( 'Wrist', '&#x2195;', 'border' ) );
+		mfolder.add( posture.wristLeft.value, 'y', -0.6, 0.4 ).name( html( '', '&#x2194;' ) );
+		mfolder.add( posture.forearmLeft.value, 'x', -1.5, 3 ).name( html( '', '&#x21BB;' ) );
+
+	}
+
+	mfolder = gui.addFolder( 'RIGHT ARM' ).close();
+	{
+
+		mfolder.add( posture.armRight.value, 'z', -1.5, 1.7 ).name( html( 'Arm', '&#x2195;' ) );
+		mfolder.add( posture.armRight.value, 'y', -2, 0.5 ).name( html( '', '&#x2194;' ) );
+		mfolder.add( posture.armRight.value, 'x', -0.5, 0.5 ).name( html( '', '&#x21BB;' ) );
+
+		mfolder.add( posture.elbowRight.value, 'y', -2.7, 0 ).name( html( 'Elbow', '&#x2195;', 'border' ) );
+
+		mfolder.add( posture.wristRight.value, 'z', -1.4, 1.4 ).name( html( 'Wrist', '&#x2195;', 'border' ) );
+		mfolder.add( posture.wristRight.value, 'y', -0.4, 0.6 ).name( html( '', '&#x2194;' ) );
+		mfolder.add( posture.forearmRight.value, 'x', -1.5, 3 ).name( html( '', '&#x21BB;' ) );
+
+	}
+
+	return gui;
+
+}
+
+
+
+function rigModel( time2 ) {
+
+	var time = time2 + Math.sin( 7*time2 )/6;
+
+	model.rotation.set( 0.5*Math.sin( time ), time, 0.5*Math.cos( time ) );
+
+	posture.waist.value.set(
+		Math.sin( time )/4-0.2,
+		Math.cos( time*1.2 )/2,
+		Math.sin( time*3 )/2.5
+	);
+
+	posture.chest.value.set(
+		Math.cos( time*3 )/3,
+		0, //08.05.25 Math.sin( time*1.2*2 )/2,
+		Math.cos( time*3*1.5 )/3
+	);
+
+	posture.head.value.set(
+		Math.cos( time*3 )/2,
+		Math.sin( time*1.2*2 )/2,
+		Math.cos( time*3*1.5 )/3
+	);
+
+	posture.kneeLeft.value.set(
+		-( Math.cos( time*3 )+1 )/1.5,
+		0,
+		0,
+	);
+
+	posture.kneeRight.value.set(
+		-( Math.sin( time*2.4 )+1 )/1.5,
+		0,
+		0,
+	);
+
+	posture.footLeft.value.set(
+		( Math.cos( time*1.7 ) )*0.5+0.15,
+		0,
+		0,
+	);
+
+	posture.footRight.value.set(
+		( Math.sin( time*1.9 ) )*0.5+0.15,
+		0,
+		0,
+	);
+
+	posture.ankleLeft.value.set(
+		Math.cos( time*3 )/2,
+		0*	Math.cos( time*2.8 )/2,
+		Math.cos( time*2.2 )/4,
+	);
+
+	posture.ankleRight.value.set(
+		Math.sin( time*3.2 )/2,
+		0*	Math.cos( time*2.3 )/2,
+		Math.sin( time*2.1 )/4,
+	);
+
+	posture.legLeft.value.set(
+		0,
+		Math.cos( time*2.8 )-0.25,
+		0,
+	);
+
+	posture.legRight.value.set(
+		0,
+		Math.cos( time*2.3 )+0.25,
+		0,
+	);
+
+	posture.hipLeft.value.set(
+		( Math.cos( time*3 )/1+0.25 ),
+		0, //08.05.25 Math.cos( time*2.8 ),///4,
+		-( Math.cos( time*2.2 )+1 )/4,
+	);
+
+	posture.hip2Left.value.set(
+		0,
+		Math.cos( time*2.8 ),
+		0,
+	);
+
+	posture.hipRight.value.set(
+		( Math.sin( time*2.2 )/1+0.25 ),
+		0, //08.05.25 Math.sin( time*3.2 )/4,
+		( Math.sin( time*2.6 )+1 )/4,
+	);
+
+	posture.hip2Right.value.set(
+		0,
+		Math.sin( time*3.2 ),
+		0,
+	);
+
+	posture.elbowLeft.value.set(
+		0,
+		( Math.cos( time*2-1 )+1 ),
+		0,
+	);
+
+	posture.elbowRight.value.set(
+		0,
+		-( Math.sin( time*1.8 )+1 ),
+		0,
+	);
+
+	posture.forearmLeft.value.set(
+		0.5*( Math.cos( time*3.6-1 )*1.5+1 ),
+		0,
+		0,
+	);
+
+	posture.forearmRight.value.set(
+		0.5*( Math.sin( time*1.8*3+1 )*1.5+1 ),
+		0,
+		0,
+	);
+
+	posture.wristLeft.value.set(
+		0,
+		( Math.cos( time*3.6-1 )*0.5+0.1 ),
+		( Math.cos( time*2.6+1 )*0.7 ),
+	);
+
+	posture.wristRight.value.set(
+		0,
+		( Math.cos( time*3.6-1 )*0.5+0.1 ),
+		-( Math.cos( time*2.6+1 )*0.7 ),
+	);
+
+	posture.armLeft.value.set(
+		0.7*( Math.cos( time*3 )/1.5-0.15 ),
+		0, //-0.7*( Math.cos( time*2.8 )*1-0.6 ),
+		0.7*( Math.cos( time*2.2 ) ),
+	);
+
+	posture.armRight.value.set(
+		0.7*( Math.sin( time*2.4 )/1.5-0.15 ),
+		0, //-0.7*-( Math.cos( time*3.1 )*1-0.6 ),
+		0.7*( Math.sin( time*2.7 ) ),
+	);
+
+}
+
+
+
+function rigRandomModel( seed = THREE.MathUtils.randInt( Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER ) ) {
+
+	THREE.MathUtils.seededRandom( seed );
+
+	function rand( from=-1, to=1 ) {
+
+		return THREE.MathUtils.seededRandom() * ( to-from ) + from;
+
+	}
+
+	model.rotation.y += rand( )-0.2;
+
+	posture.waist.value.set(
+		rand()/4-0.2,
+		rand()/2,
+		rand()/2.5
+	);
+
+	posture.chest.value.set(
+		rand()/3,
+		0,
+		rand( )/3
+	);
+
+	posture.head.value.set(
+		rand( )/2,
+		rand( )/2,
+		rand( )/3
+	);
+
+	posture.kneeLeft.value.set(
+		-( rand( )+1 )/1.5,
+		0,
+		0,
+	);
+
+	posture.kneeRight.value.set(
+		-( rand( )+1 )/1.5,
+		0,
+		0,
+	);
+
+	posture.footLeft.value.set(
+		( rand( ) )*0.5+0.15,
+		0,
+		0,
+	);
+
+	posture.footRight.value.set(
+		( rand( ) )*0.5+0.15,
+		0,
+		0,
+	);
+
+	posture.ankleLeft.value.set(
+		rand( )/2,
+		0,
+		rand( )/4,
+	);
+
+	posture.ankleRight.value.set(
+		rand( )/2,
+		0,
+		rand( )/4,
+	);
+
+	posture.legLeft.value.set(
+		0,
+		rand( )-0.25,
+		0,
+	);
+
+	posture.legRight.value.set(
+		0,
+		rand( )+0.25,
+		0,
+	);
+
+	posture.hipLeft.value.set(
+		( rand( )/1+0.25 ),
+		0,
+		-( rand( )+1 )/4,
+	);
+
+	posture.hip2Left.value.set(
+		0,
+		rand( ),
+		0,
+	);
+
+	posture.hipRight.value.set(
+		( rand( )/1+0.25 ),
+		0,
+		( rand( )+1 )/4,
+	);
+
+	posture.hip2Right.value.set(
+		0,
+		rand( ),
+		0,
+	);
+
+	posture.elbowLeft.value.set(
+		0,
+		( rand( )+1 ),
+		0,
+	);
+
+	posture.elbowRight.value.set(
+		0,
+		-( rand( )+1 ),
+		0,
+	);
+
+	posture.forearmLeft.value.set(
+		0.5*( rand( )*1.5+1 ),
+		0,
+		0,
+	);
+
+	posture.forearmRight.value.set(
+		0.5*( rand( )*1.5+1 ),
+		0,
+		0,
+	);
+
+	posture.wristLeft.value.set(
+		0,
+		( rand( )*0.5+0.1 ),
+		( rand( )*0.7 ),
+	);
+
+	posture.wristRight.value.set(
+		0,
+		( rand( )*0.5+0.1 ),
+		-( rand( )*0.7 ),
+	);
+
+	posture.armLeft.value.set(
+		0.7*( rand( )/1.5-0.15 ),
+		0,
+		0.7*( rand( ) ),
+	);
+
+	posture.armRight.value.set(
+		0.7*( rand( )/1.5-0.15 ),
+		0,
+		0.7*( rand( ) ),
+	);
+
+	updateGUI( );
+
+}
+
+
+
+function showPivotPoint( index ) {
+
+	model.add( pivot );
+	switch ( index ) {
+
+		case 1: pivot.position.copy( skeleton.head.pivot ); break;
+		case 2: pivot.position.copy( skeleton.chest.pivot ); break;
+		case 3: pivot.position.copy( skeleton.waist.pivot ); break;
+
+		case 11: pivot.position.copy( skeleton.hip.pivot ); break;
+		case 15: pivot.position.copy( skeleton.hip2.pivot ); break;
+		case 12: pivot.position.copy( skeleton.leg.pivot ); break;
+		case 13: pivot.position.copy( skeleton.knee.pivot ); break;
+		case 14: pivot.position.copy( skeleton.ankle.pivot ); break;
+		case 16: pivot.position.copy( skeleton.foot.pivot ); break;
+
+		case 21: pivot.position.copy( skeleton.arm.pivot ); break;
+		case 22: pivot.position.copy( skeleton.elbow.pivot ); break;
+		case 23: pivot.position.copy( skeleton.forearm.pivot ); break;
+		case 24: pivot.position.copy( skeleton.wrist.pivot ); break;
+
+		default: model.remove( pivot );
+
+	}
+
+}
+
+
+
+
+function changePivotPoint( ) {
+
+	//	pivot.position.copy( skeleton.hipLeftPos.value );
+
+}
+
+
+
+function updateGUI( ) {
+
+	for ( var ctrl of gui.controllersRecursive() ) ctrl.updateDisplay( );
+
+}
+
+
+
+function animationLoop( t ) {
+
+	if ( options.animate ) {
+
+		rigModel( t/1000 );
+		updateGUI( );
+
+	}
+
+	controls.update( );
+	light.position.copy( camera.position );
+
+	renderer.render( scene, camera );
+
+}
+
+
+renderer.setAnimationLoop( animationLoop );
+
+
+if ( DEBUG ) {
+
+	setTimeout( ()=>{
+
+		showPivotPoint( 11 ); changePivotPoint();
+
+	}, 500 );
+
+}
+
+
+export { createGui, scene };
